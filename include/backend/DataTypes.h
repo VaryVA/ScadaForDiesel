4ec9aec26c560182a2c09963715203469db50661
Lenar Gatin <lgatin1711@gmail.com>
2026-05-05 21:26:49 +0500

    chore: move the ModbusConfig to DataTypes.h and restructure modbus client

--- include/backend/DataTypes.h
+++ include/backend/DataTypes.h
@@ -1,5 +1,50 @@
 #pragma once
 #include <QtGlobal>
+#include <QString>
+
+// Control registers mapping, read/write, 1-bit
+struct CoilsRegistersScheme
+{
+    bool motorEnabled;
+};
+
+// Device status registers mapping, only read, 1-bit
+struct DiscreteRegistersScheme
+{
+    bool fanAd;
+    bool fanBall;
+};
+
+// Settings and variables registers mapping, read/write, 16-bit
+struct HoldingRegistersScheme
+{
+    double targetRpm;
+    double throttlePosition;
+    double brakeTorque;
+};
+
+// Sensor data registers mapping, only read, 16-bit
+struct InputRegistersScheme
+{
+    double rpm, torque;
+    double dieselTemp, motorTemp, resistorTemp, dieselPressure;
+    double throttle, brakeTorque;
+};
+
+struct ModbusConfig
+{
+    QString host;
+    quint16 port = 1502;
+    int pollFrequencyMs = 1000;
+    int timeoutMs = 1000;
+    int retries = 3;
+    int unitId = 1;  // we assume one modbus device, so unitId will be ignored
+    // Start address is 0 for each register type
+    CoilsRegistersScheme coils;
+    DiscreteRegistersScheme discrete;
+    InputRegistersScheme input;
+    HoldingRegistersScheme holding;
+};
 
 // Settings of model (for writing/reading operations)
 struct ModelConfig
--- include/backend/modbus_client/config/MockModbusConfigBuilder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "IModbusConfigBuilder.h"
-
-class MockModbusConfigBuilder : public IModbusConfigBuilder
-{
-public:
-    std::optional<ModbusConfig> get() const override;
-};
